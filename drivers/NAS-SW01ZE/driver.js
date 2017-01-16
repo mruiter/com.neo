8644515b88360f206234f30303a53ccff93f11fc
mruiter <mruiter@tuxweb.nl>
2017-01-16 13:30:01 +0100

    Touch Driver Change

--- drivers/NAS-SW01ZE/driver.js
+++ drivers/NAS-SW01ZE/driver.js
@@ -10,9 +10,11 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			'command_class': 'COMMAND_CLASS_SWITCH_BINARY',
 			'command_get': 'SWITCH_BINARY_GET',
 			'command_set': 'SWITCH_BINARY_SET',
-			'command_set_parser': value => ({
-				'Switch Value': (value > 0) ? 'on/enable' : 'off/disable'
-			}),
+			'command_set_parser': value => {
+				return {
+					'Switch Value': (value > 0) ? 255 : 0
+				};
+			},
 			'command_report': 'SWITCH_BINARY_REPORT',
 			'command_report_parser': report => report['Value'] === 'on/enable'
 		}
