0955691e74e2e0692b1666d25381944368d02a81
dT <TedTolboom@users.noreply.github.com>
2017-01-21 21:37:47 +0100

    Update for 1.1.3 (GetOnWakeUp:true)
    See https://github.com/athombv/node-homey-zwavedriver/pull/26/files 
    
    Should be changed in Homey's core at 1.1.3

--- drivers/NAS-PD01ZE/driver.js
+++ drivers/NAS-PD01ZE/driver.js
@@ -48,7 +48,7 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
    					}		
     	},
 		'measure_battery': { 
-    			//getOnWakeUp: true,
+    			getOnWakeUp: true,
     			'command_class': 'COMMAND_CLASS_BATTERY',
 	    		'command_get': 'BATTERY_GET',
     			'command_report': 'BATTERY_REPORT',
@@ -110,22 +110,4 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			"parser": value => new Buffer([ ( value === true ) ? 1 : 0 ])
 		}
 	}
-});
-
-module.exports.on('initNode', token => {
-    const node = module.exports.nodes[token];
-    if (node) {
-        if (node.instance.CommandClass.COMMAND_CLASS_WAKE_UP) {
-            node.instance.CommandClass.COMMAND_CLASS_WAKE_UP.on('report', (command, report) => {
-                if (command.name === 'WAKE_UP_NOTIFICATION') {
-                    // Retrieve 'Battery Level' upon wake-up; temp replacement of getOnWakeUp function (triggering on online event instead of Wake-up)
-                    	node.instance.CommandClass['COMMAND_CLASS_BATTERY'].BATTERY_GET({});
-                    // Option to retrieve the WAKE_UP_INTERVAL upon wake-up; for debugging only
-                    	//node.instance.CommandClass['COMMAND_CLASS_WAKE_UP'].WAKE_UP_INTERVAL_GET({});
-                    // Option to retrieve configuration of a certain parameter upon wake-up; for debugging only
-                    	//node.instance.CommandClass['COMMAND_CLASS_CONFIGURATION'].CONFIGURATION_GET( {'Parameter Number': new Buffer([10])}, null );
-                }
-            });
-        }
-    }
 });
