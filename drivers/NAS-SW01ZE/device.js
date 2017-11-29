3ca0615ccda475293102bb03d379883eb934f729
Marco Ruiter <mruiter@tuxweb.nl>
2017-11-29 16:08:34 +0100

    Revert "Led Control Flow Card"
    This reverts commit cac98f9eeca352131fca445e12002db4874a14c2.

--- drivers/NAS-SW01ZE/device.js
+++ drivers/NAS-SW01ZE/device.js
@@ -2,22 +2,12 @@
 
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-class WallSwitchDual_SW01Z extends ZwaveDevice {
+class WallSwitchDual_WS01Z extends ZwaveDevice {
 	async onMeshInit() {
 		//this.enableDebug();
 		//this.printNode();
 		this.registerCapability('onoff', 'SWITCH_BINARY');
-		
-		//===== Led On/Off
-		let SW01ZE_LED_mode_run_listener = async(args) => {
-			this.log('FlowCardAction Set Led Mode to: ', args.led_mode);
-			this.configurationSet({id: 'backlight'}, args.led_mode);
-		};
-		let actionSW01ZE_led_mode = new Homey.FlowCardAction('SW01ZE_led_mode');
-		actionSW01ZE_led_mode
-			.register()
-			.registerRunListener(SW01ZE_LED_mode_run_listener);		
 	}
 }
 
-module.exports = WallSwitchDual_SW01Z;
+module.exports = WallSwitchDual_WS01Z;
