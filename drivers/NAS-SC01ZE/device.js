428babbe74513341c02e679fb53af7f0e6f9c68e
Marco Ruiter <mruiter@tuxweb.nl>
2017-11-29 16:51:27 +0100

    Revert "Cleanup"
    This reverts commit 6f0a2d03c7a3e492bee8cb5335340cb3827acb0c.

--- drivers/NAS-SC01ZE/device.js
+++ drivers/NAS-SC01ZE/device.js
@@ -7,5 +7,16 @@ class WallSwitchDual_SC01Z extends ZwaveDevice {
 		//this.enableDebug();
 		//this.printNode();
 		this.registerCapability('onoff', 'SWITCH_BINARY');
+		
+		//===== Led On/Off
+		let SC01ZE_LED_mode_run_listener = async(args) => {
+			this.log('FlowCardAction Set Led Mode to: ', args.led_mode);
+			this.configurationSet({id: 'backlight'}, args.led_mode);
+		};
+		let actionSC01ZE_led_mode = new Homey.FlowCardAction('SC01ZE_led_mode');
+		actionSC01ZE_led_mode
+			.register()
+			.registerRunListener(SC01ZE_LED_mode_run_listener);		
+	}
 }
 module.exports = WallSwitchDual_SC01Z;
--- drivers/NAS-SW01ZE/device.js
+++ drivers/NAS-SW01ZE/device.js
@@ -7,6 +7,16 @@ class WallSwitchDual_SW01Z extends ZwaveDevice {
 		//this.enableDebug();
 		//this.printNode();
 		this.registerCapability('onoff', 'SWITCH_BINARY');
+		
+		//===== Led On/Off
+		let SW01ZE_LED_mode_run_listener = async(args) => {
+			this.log('FlowCardAction Set Led Mode to: ', args.led_mode);
+			this.configurationSet({id: 'backlight'}, args.led_mode);
+		};
+		let actionSW01ZE_led_mode = new Homey.FlowCardAction('SW01ZE_led_mode');
+		actionSW01ZE_led_mode
+			.register()
+			.registerRunListener(SW01ZE_LED_mode_run_listener);		
 	}
 }
 
