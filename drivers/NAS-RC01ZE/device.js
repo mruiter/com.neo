74d9582649a915d5aaca918080fced78960e59a4
Marco Ruiter <mruiter@tuxweb.nl>
2017-12-02 15:08:40 +0100

    Keyfob Working

--- drivers/NAS-RC01ZE/device.js
+++ drivers/NAS-RC01ZE/device.js
@@ -18,40 +18,30 @@ class KeyFob_RC01Z extends ZwaveDevice {
 		triggerRC_scene
 			.register()
 			.registerRunListener((args, state) => {
-				this.log(args, state);
+				//this.log(args, state);
 				return Promise.resolve(args.button === state.button && args.scene === state.scene);
 			});
 
-		let triggerRC_button = new Homey.FlowCardTriggerDevice('RC_button');
-		triggerRC_button
-			.register();
 		
 		// register a report listener (SDK2 style not yet operational)
 		this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (rawReport, parsedReport) => {
-			this.log('registerReportListener', rawReport, parsedReport);
-		});		
-		
-		// OLD API reportListener used since new registerReportListener is not active without capability
-		this.node.CommandClass['COMMAND_CLASS_CENTRAL_SCENE'].on('report', (command, report) => {
-			if (command.name === 'CENTRAL_SCENE_NOTIFICATION' &&
-				report &&
-				report.hasOwnProperty('Properties1') &&
-				report.Properties1.hasOwnProperty('Key Attributes') &&
-				report.hasOwnProperty('Scene Number') &&
-				report.hasOwnProperty('Sequence Number')) {
-				if (report['Sequence Number'] !== PreviousSequenceNo) {
+			if (rawReport.hasOwnProperty('Properties1') &&
+				rawReport.Properties1.hasOwnProperty('Key Attributes') &&
+				rawReport.hasOwnProperty('Scene Number') &&
+				rawReport.hasOwnProperty('Sequence Number')) {
+				if (rawReport['Sequence Number'] !== PreviousSequenceNo) {
 					const remoteValue = {
-						button: report['Scene Number'].toString(),
-						scene: report.Properties1['Key Attributes'],
+						button: rawReport['Scene Number'].toString(),
+						scene: rawReport.Properties1['Key Attributes'],
 					};
-					PreviousSequenceNo = report['Sequence Number'];
+					PreviousSequenceNo = rawReport['Sequence Number'];
+					this.log('Triggering sequence:', PreviousSequenceNo, 'remoteValue', remoteValue);
 					// Trigger the trigger card with 2 dropdown options
 					triggerRC_scene.trigger(this, triggerRC_scene.getArgumentValues, remoteValue);
-					// Trigger the trigger card with tokens
-					triggerRC_button.trigger(this, remoteValue, null);
 				}
 			}
-		});	
+		});		
+
 		
 		
 	}
