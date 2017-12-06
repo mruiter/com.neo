e4b70f735ff92ee18716a6b988470a32cb575151
Marco Ruiter <mruiter@tuxweb.nl>
2017-12-06 19:39:30 +0100

    card

--- drivers/NAS-AB01ZE/device.js
+++ drivers/NAS-AB01ZE/device.js
@@ -5,8 +5,8 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class Siren_AB01Z extends ZwaveDevice {
   async onMeshInit() {
-    this.enableDebug();
-    this.printNode();
+    //this.enableDebug();
+    //this.printNode();
     this.registerCapability('onoff', 'SWITCH_BINARY');
     this.registerCapability('alarm_battery', 'BATTERY');
     this.registerCapability('measure_battery', 'BATTERY', {
@@ -38,10 +38,10 @@ class Siren_AB01Z extends ZwaveDevice {
 
 	//===== Create a Action card to activate or deactivated the siren
     let AB01ZE_alarm_state_run_listener = async (args) => {
-      this.log('Status Alarm: ', alarm_state);
-	 	return this.triggerCapabilityListener('onoff', alarm_state)
+      this.log('Status Alarm: ', args.alarm_state);
+	 	return this.triggerCapabilityListener('onoff', args.alarm_state)
 				.then(() => null)
-				.catch(err => new Error('failed_to_trigger_onoff'));
+				.catch(err => new Error('failed_to_trigger_siren'));
 	};
 
     let actionAB01ZE_alarm_state = new Homey.FlowCardAction('AB01ZE_alarm_state');
@@ -51,7 +51,7 @@ class Siren_AB01Z extends ZwaveDevice {
 	
 	
 	
-	// Cards that responde to the siren activating
+	// Cards that responde to the siren activating / blink icon alarm
     // Register Flow card trigger
     const SirenFlowTrigger = new Homey.FlowCardTriggerDevice('alarm_siren');
     SirenFlowTrigger.register();
@@ -70,6 +70,7 @@ class Siren_AB01Z extends ZwaveDevice {
       });
     } else this.error('missing_alarm_siren_card_in_manifest');
 
+	// Cards that change device settings
     //===== CONTROL Siren Alarm/Doorbell mode
     let AB01ZE_alarm_mode_run_listener = async (args) => {
       this.log('FlowCardAction Set Alarm Mode to: ', args.alarm_mode);
