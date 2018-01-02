c1cb116183a604d263c9209305490435d82f9244
Marco Ruiter <mruiter@tuxweb.nl>
2018-01-02 15:07:30 +0100

    fix

--- drivers/NAS-WS01ZE/device.js
+++ drivers/NAS-WS01ZE/device.js
@@ -7,25 +7,7 @@ class WaterSensor_WS01 extends ZwaveDevice {
   async onMeshInit() {
     this.enableDebug();
     this.printNode();
-    this.registerCapability('alarm_water', 'NOTIFICATION', {
-      get: 'NOTIFICATION_GET',
-      getOpts: {
-        getOnOnline: true,
-      },
-      getParser: () => ({
-        'V1 Alarm Type': 0,
-        'Notification Type': 'Water',
-      }),
-      report: 'NOTIFICATION_REPORT',
-      reportParser: report => {
-        if (report && report['Notification Type'] === 'Water') {
-          if (report['Event'] === 1) return true;
-          if (report['Event'] === 0) return false;
-        }
-        return null;
-      }
-    });
-	
+    this.registerCapability('alarm_water', 'SENSOR_BINARY');
     this.registerCapability('alarm_battery', 'BATTERY');
     this.registerCapability('measure_battery', 'BATTERY', {
       getOpts: {
