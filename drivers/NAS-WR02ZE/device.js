527e49bab0cd39ed93c3ab54ce4ec5b2a3ade5a9
Marco Ruiter <mruiter@tuxweb.nl>
2019-10-22 21:23:24 +0200

    Fixed parser v4 for v2 plug

--- drivers/NAS-WR02ZE/device.js
+++ drivers/NAS-WR02ZE/device.js
@@ -5,13 +5,85 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class Wallplug_WR02Z extends ZwaveDevice {
   onMeshInit() {
-    // this.enableDebug();
-    // this.printNode();
+    this.enableDebug();
+    this.printNode();
     this.registerCapability('onoff', 'SWITCH_BINARY');
-	this.registerCapability('meter_power', 'METER');
-    this.registerCapability('measure_power', 'METER');
-	this.registerCapability('measure_current', 'METER');
-    this.registerCapability('measure_voltage', 'METER');
+
+		this.registerCapability('measure_power', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 2,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 2 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === false) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+		});
+
+        this.registerCapability('meter_power', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 0,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 0 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === false) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+        });
+
+        this.registerCapability('measure_voltage', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 4,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 0 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === true) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+        });
+
+        this.registerCapability('measure_current', 'METER', {
+            getParserV4: () => ({
+                'Sensor Type': 'Electric meter',
+                'Properties1': {
+                    'Scale': 5,
+                }
+            }),
+            reportParserV4: report => {
+                if (report.hasOwnProperty('Properties2') &&
+                    report.Properties2.hasOwnProperty('Scale bits 10') &&
+                    report.Properties2['Scale bits 10'] === 1 &&
+                    report.Properties1.hasOwnProperty('Scale bit 2') &&
+                    report.Properties1['Scale bit 2'] === true ) {
+                    return report['Meter Value (Parsed)'];
+                }
+                return null;
+            }
+        });
 
 	// define FlowCardAction to reset meter_power
 		let WR02Z_reset_meter_run_listener = async(args) => {
