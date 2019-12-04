97da03d7ab9b724d5c79ae60a352555430f1507e
Marco Ruiter <mruiter@tuxweb.nl>
2019-12-04 14:17:38 +0100

    Added new flood sensor id

--- drivers/NAS-CS01ZE/device.js
+++ drivers/NAS-CS01ZE/device.js
@@ -3,23 +3,23 @@
 const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-class WallSwitchDual_SC03ZE extends ZwaveDevice {
+class CurtainController_CS01ZE extends ZwaveDevice {
   async onMeshInit() {
     //this.enableDebug();
     //this.printNode();
     this.registerCapability('onoff', 'SWITCH_BINARY');
 
     //===== Led On/Off
-    let SC03ZE_LED_mode_run_listener = async (args) => {
+    let CS01ZE_LED_mode_run_listener = async (args) => {
       this.log('FlowCardAction Set Led Mode to: ', args.switch_LED_onoff);
       this.configurationSet({
         id: 'backlight'
       }, args.switch_LED_onoff);
     };
-    let actionSC03ZE_led_mode = new Homey.FlowCardAction('SC03ZE_switch_LED');
-    actionSC03ZE_led_mode
+    let actionCS01ZE_led_mode = new Homey.FlowCardAction('CS01ZE_switch_LED');
+    actionCS01ZE_led_mode
       .register()
-      .registerRunListener(SC03ZE_LED_mode_run_listener);
+      .registerRunListener(CS01ZE_LED_mode_run_listener);
   }
 }
-module.exports = WallSwitchDual_SC03ZE;
+module.exports = CurtainController_CS01ZE;
