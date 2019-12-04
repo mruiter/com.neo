986febcc44009c2c11b36949030426b30821a411
Marco Ruiter <mruiter@tuxweb.nl>
2019-12-04 15:35:14 +0100

    Try for curtain controller

--- drivers/NAS-CS01ZE/device.js
+++ drivers/NAS-CS01ZE/device.js
@@ -7,19 +7,9 @@ class CurtainController_CS01ZE extends ZwaveDevice {
   async onMeshInit() {
     //this.enableDebug();
     //this.printNode();
-    this.registerCapability('onoff', 'SWITCH_BINARY');
+	this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+	this.registerCapability('windowcoverings_state', 'SWITCH_BINARY');
 
-    //===== Led On/Off
-    let CS01ZE_LED_mode_run_listener = async (args) => {
-      this.log('FlowCardAction Set Led Mode to: ', args.switch_LED_onoff);
-      this.configurationSet({
-        id: 'backlight'
-      }, args.switch_LED_onoff);
-    };
-    let actionCS01ZE_led_mode = new Homey.FlowCardAction('CS01ZE_switch_LED');
-    actionCS01ZE_led_mode
-      .register()
-      .registerRunListener(CS01ZE_LED_mode_run_listener);
   }
 }
 module.exports = CurtainController_CS01ZE;
