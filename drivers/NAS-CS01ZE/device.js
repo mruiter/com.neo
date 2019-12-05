9f50055af673fbc5679db722053fe53efea45241
Marco Ruiter <mruiter@tuxweb.nl>
2019-12-05 07:56:07 +0100

    Enabled debug on curtain controller

--- drivers/NAS-CS01ZE/device.js
+++ drivers/NAS-CS01ZE/device.js
@@ -5,8 +5,8 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class CurtainController_CS01ZE extends ZwaveDevice {
   async onMeshInit() {
-    //this.enableDebug();
-    //this.printNode();
+    this.enableDebug();
+    this.printNode();
 	this.registerCapability('dim', 'SWITCH_MULTILEVEL');
 	this.registerCapability('windowcoverings_state', 'SWITCH_BINARY');
 
