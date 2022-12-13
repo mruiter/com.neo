f91da0e632dca786037bc67b7153b0114d448679
WJGvdVelden <86908572+WJGvdVelden@users.noreply.github.com>
2022-12-13 16:34:14 +0100

    Add files via upload

---
+++ drivers/NAS-PD01ZEv2/device.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Homey = require('homey');
+const { ZwaveDevice } = require('homey-zwavedriver');
+
+class MultiSensor_PD01Z extends ZwaveDevice {
+
+  async onNodeInit() {
+    // enable debugging
+    // this.enableDebug();
+
+    // print the node's info to the console
+    // this.printNode();
+
+    // register device capabilities
+    this.registerCapability('alarm_motion', 'NOTIFICATION');
+    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+	  this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
+    this.registerCapability('measure_battery', 'BATTERY');
+  }
+}
+module.exports = MultiSensor_PD01Z;
